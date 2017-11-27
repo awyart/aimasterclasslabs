6ce043981185a0d0485d59ced40b83296c55da98
Amaury WYART <awyart@student.42.fr>
2017-11-27 17:19:59 +0100

    2 convolutions 2 lineaires

--- models.py
+++ models.py
@@ -4,15 +4,25 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.fc0 = nn.Linear(28*28,200)
-        self.fc1 = nn.Linear(200,100)
-        self.fc2 = nn.Linear(100,27)
+        self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.bn1 = nn.BatchNorm2d(20, eps=1e-05, momentum=0.1, affine=True)
+        self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        self.bn2 = nn.BatchNorm2d(50, eps=1e-05, momentum=0.1, affine=True)
+        self.fc0 = nn.Linear(4*4*50,100)
+        self.fc1 = nn.Linear(100,27)
 
 
     def forward(self, x):
+        x = self.conv1(x)
+        x = F.max_pool2d(x,2,2)
+        x = self.bn1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.max_pool2d(x,2,2)
+        x = self.bn2(x)
+        x = F.relu(x)
         x = self.fc0(x.view(x.size(0), -1))
         x = self.fc1(x.view(x.size(0), -1))
-        x = self.fc2(x.view(x.size(0), -1))
 
         return F.log_softmax(x)
 
